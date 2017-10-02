0c959997e368397f1ec8b87527e87284d9db0843
Denis <dhristov99626@gmail.com>
2017-10-02 11:26:24 +0300

    Changed a simple error

--- src/com/phonebook/Person.java
+++ src/com/phonebook/Person.java
@@ -41,7 +41,7 @@ public class Person {
 
 	static boolean isFrom2to9(String str) {
 		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) < '2' || str.charAt(i) > '9') {
+			if (str.charAt(i) < '0' || str.charAt(i) > '9') {
 				return false;
 			}
 		}
